ea52a6bc7882dc6f880a6b9910762b3a20526df9
alicia0117 <alicia0117@naver.com>
2023-10-15 21:46:59 +0900

    실습 3-3

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square (int n)
-{
-    return n*n;
+int get_max(int x, int y) {
+    if (x>y) {
+       return x;
+} else {
+       return y;
+       }
 }
-int main(int argc, char *argv[])
-{
-    int n;
+
+int main(int argc, char *argv[]) {
+    int a, b;
     
-    n = 4;
-    printf("square result: %i\n", square(n));
+    a= 3;
+    b= 10;
     
+    printf("get_max result: %i\n", get_max(a,b));
     
     
   system("PAUSE");	
