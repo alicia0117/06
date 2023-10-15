7ebe73de975727e5cac0e9ba20e78745ee238039
alicia0117 <alicia0117@naver.com>
2023-10-15 21:30:03 +0900

    실습 5

--- main.c
+++ main.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_star()
-{
- int i;
- for( i = 0; i < 10; i++ )
-printf("*");
+int factorial(int n) {
+    int i, res=1;
+    for (i +1; i<+ n; i++) {
+        res = res*i;
+    }
+    return res;
 }
 
-int main(void)
-{
- print_star();
- print_star();
- print_star();
+int combination(int n, int r){
+    int div1, div2;
+    
+    div1= factorial(n);
+    div2= factorial(n-r) * factorial (r);
+    
+ return (div1/div2);
+}
 
+int main(int argc, char *argv[])
+{
+    int n, r;
+    int result;
+    
+    printf("input n : ");
+    
+    scanf("%d", &n);
+    
+    printf("input r : ");
+    
+    scanf("%d", &r);
+    
+    result = combination (n, r);
+    
+    printf("result: %d\n", result);
+    
+    
   system("PAUSE");	
   return 0;
 }
