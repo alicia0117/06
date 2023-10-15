38a413e947d285ffb6b63ac241cea0abfb91720f
alicia0117 <alicia0117@naver.com>
2023-10-15 20:54:37 +0900

    실습 2

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void func (void)
+{
+     int x;
+     printf("func x is at %p\n", &x);
+     }
+     
+int main(int argc, char *argv[])
+{
+    int x;
+    
+    printf("main x is at %p\n", &x);
+    func();
+  
+  system("PAUSE");	
+  return 0;
+}
