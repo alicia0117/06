55ae596af42c459d2ebd8aa615f14f92cce289e5
alicia0117 <alicia0117@naver.com>
2023-10-15 21:41:44 +0900

    실습 3-2

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumTwo (int a, int b)
+int square (int n)
 {
-    int result= a+b;
-    return result;
-    
+    return n*n;
 }
-
-    int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int a, b;
-    a=3;
-    b=5;
-    printf("sumTwo result: %i\n", sumTwo(a,b));
+    int n;
+    
+    n = 4;
+    printf("square result: %i\n", square(n));
+    
     
     
   system("PAUSE");	
